14f428dc372ea3ed337aac80f8ba38f0445d6e89
Ace <PC@HONGCF>
2025-10-06 22:12:24 +0800

    pdf

--- config.js
+++ config.js
@@ -2,8 +2,11 @@
 window.APP_CONFIG = {
   // API基础地址
   API_BASE_URL: 'https://api.openai2025.com/api',
-  // 完整API地址（用于下载等）
+  // // 完整API地址（用于下载等）
   API_FULL_URL: 'https://api.openai2025.com',
+  //API_BASE_URL: 'http://localhost:5162/api',
+  // 完整API地址（用于下载等）
+  //API_FULL_URL: 'http://localhost:5162',
   // 环境标识
   ENV: 'production'
 };
